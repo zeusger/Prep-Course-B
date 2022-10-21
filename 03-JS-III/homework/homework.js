165dc4cc1342379cb538382cade765a0a8f8956f
daiana vilches <germanmercadook@gmail.com>
2022-10-21 18:52:04 -0300

    status

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,53 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var acumulador = 0;
+    for (var i = 1; i < 11; i++ ){
+      acumulador = acumulador + i;
+    }
+    return acumulador;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var resultado = [];
+  var indice = 0;
+  while(indice < array.length){
+    if (array[indice]%2===0){
+      resultado.push(array[indice])
+    }
+    indice++;
+  }
+  return resultado;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map(function(num){
+    return Math.pow(num,2);
+  })
+
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+var suma = array.reduce(function(acumulador,num){
+    return acumulador + num ;
+}, 0)
+return suma
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var num = num + "";
+  return num.length;
 }
   
   
